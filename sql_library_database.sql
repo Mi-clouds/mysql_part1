627f4c3199a4bcea4bcb49c879225203e815b9e9
Michelle <mistrymichelle@gmail.com>
2021-03-22 19:11:19 +0000

    adding library_db

--- sql_library_database.sql
+++ sql_library_database.sql
@@ -1,81 +1,53 @@
--- Create database userstories;
--- use userstories;
--- create table appusers
--- (
--- user_id int auto_increment primary key,
--- usertype varchar(100) not null,
--- goals varchar(200)
+CREATE DATABASE IF NOT EXISTS userstories DEFAULT CHARSET = utf8;
+USE userstories;
 
-foreign key (user_id) references usermotives (reason_id)
--- ); 
+CREATE TABLE appusers (
+ID int auto_increment primary key,
+usertype varchar(100) not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ALTER TABLE appusers
--- RENAME COLUMN user_id TO ID;
+-- select *
+-- from appusers;
 
--- use userstories;
+INSERT INTO appusers (usertype)
+VALUES ('Librarian'),
+('Volunteer'),
+('Library user'),
+('Special needs library user'),
+('Student user'),
+('Junior library user');
 
--- ALTER table appusers
--- drop column goals;
+CREATE TABLE usermotives (
+ID_No int auto_increment primary key,
+reasons varchar (200) not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- INSERT INTO appusers (usertype)
--- VALUES ('Librarian'),
--- ('Librarian'),
--- ('library user'),
--- ('special needs library user'),
--- ('student user'),
--- ('junior library user');
+INSERT INTO usermotives (reasons)
+VALUES ('so that they are able to be easily accessed by people'),
+('so that I can see if their loans have been paid'),
+('so that the right librarian language support is provided'),
+('so that I can monitor their useage and renew books online before overdue fines are issued'),
+('so that I can attend an event/class'),
+('so that I can borrow some books'),
+('so that I can find all books related to that subject or title'),
+('so that I can use it in my native language'),
+('so that I can see whenever I am close to having a book overdue'),
+('so that I can visit the library when it\'s open'),
+('so that I can still loan library items even though I am housebound'),
+('so that I can take more books out than regular users'),
+('so that I can input it and be charged less if I have a book overdue'),
+('so that I can read only age-appropriate books'),
+('so that I can help at the library and gain skills for my CV');
 
--- UPDATE appusers
--- SET usertype = 'volunteer'
--- WHERE user_id = 2;
 
--- delete from team
--- where team_id in (2, 6);
-
-select *
-from appusers;
-
--- use userstories;
--- create table usermotives
--- (
--- reason_ID int auto_increment primary key,
--- reasons varchar (200) not null
-foreign key (reason_ID) references usertypes (user_id)
--- );
-
-ALTER TABLE usermotives
-RENAME COLUMN reason_id TO ID_No;
-
--- INSERT INTO usermotives (reasons)
--- VALUES ('so that they are able to be easily accessed by people'),
--- ('so that I can see if their loans have been paid'),
--- ('so that the right librarian language support is provided'),
--- ('so that I can monitor their useage and renew books online before overdue fines are issued'),
--- ('so that I can attend an event/class'),
--- ('so that I can borrow some books'),
--- ('so that I can find all books related to that subject or title'),
--- ('so that I can use it in my native language'),
--- ('so that I can see whenever I am close to having a book overdue'),
--- ('so that I can visit the library when it\'s open'),
--- ('so that I can still loan library items even though I am housebound'),
--- ('so that I can take more books out than regular users'),
--- ('so that I can input it and be charged less if I have a book overdue'),
--- ('so that I can read only age-appropriate books');
-
-select *
-from usermotives
-
-use userstories;
-
-create table usergoals
-(
+DROP TABLE IF EXISTS usergoals; 
+CREATE TABLE usergoals (
 goals varchar (200) not null,
 user_ID int not null,
 reason_ID int not null,
 foreign key (reason_ID) references usermotives (ID_No),
 foreign key (user_ID) references appusers (ID)
-);
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 
 INSERT INTO usergoals (goals, user_ID, reason_ID)
 VALUES ('I want to be able to organise the books', 1, 1),
@@ -93,14 +65,15 @@ VALUES ('I want to be able to organise the books', 1, 1),
 ('I want to access BSL support', 4, 8),
 ('I want to be able to input my student status', 5, 12),
 ('I want a student specific code given to me', 5, 13),
-('I want to add my date of birth during registration and have my age calculated', 6, 14);
+('I want to add my date of birth during registration and have my age calculated', 6, 14),
+('I want to inidate my interest in volunteering at the library', 2, 15);
 
--- UPDATE usergoals
--- SET user_ID = 3
--- where reason_id in (4, 10);
+-- select *
+-- from usermotives
 
--- drop table usergoals
+-- select *
+-- from usergoals
 
-
-select *
-from usergoals
+-- DROP TABLE usergoals; 
+-- DROP TABLE usermotives;
+-- DROP TABLE appusers;
