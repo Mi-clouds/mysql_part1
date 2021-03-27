fb2e6c9a4cb3b56c79cba639a8973e645b971e1c
Michelle <mistrymichelle@gmail.com>
2021-03-27 21:29:55 +0000

    Adding publishing year table

---
+++ week_2/publishing_year_table.sql
@@ -0,0 +1,41 @@
+create database if not exists library;
+use library;
+
+create table if not exists publishing_year_table(
+year_published_id int auto_increment not null primary key,
+year_published varchar(30) not null
+);
+
+insert into year_published (version) values ('1937');
+insert into year_published (version) values ('1953');
+insert into year_published (version) values ('1976');
+insert into year_published (version) values ('1991');
+insert into year_published (version) values ('1994');
+insert into year_published (version) values ('1997');
+insert into year_published (version) values ('2003');
+insert into year_published (version) values ('2007');
+insert into year_published (version) values ('2009');
+insert into year_published (version) values ('2010');
+insert into year_published (version) values ('2011');
+insert into year_published (version) values ('2012');
+insert into year_published (version) values ('2014');
+insert into year_published (version) values ('2015');
+insert into year_published (version) values ('2016');
+insert into year_published (version) values ('2017');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- week_2/resource_type_table.sql
+++ week_2/resource_type_table.sql
@@ -6,13 +6,13 @@ resource_type_id int(2) auto_increment not null primary key,
 resource_type varchar(30) not null
 );
 
-insert into resource_version (version) values ('Paperback Book');
-insert into resource_version (version) values ('Video recordings for the hearing impaired');
-insert into resource_version (version) values ('Audiobook');
-insert into resource_version (version) values ('Large print');
-insert into resource_version (version) values ('Textbook');
-insert into resource_version (version) values ('Hardback book');
-insert into resource_version (version) values ('Textbook');
-insert into resource_version (version) values ('Braille Book for visually impaired');
-insert into resource_version (version) values ('eBook');
+insert into resource_type (version) values ('Paperback Book');
+insert into resource_type (version) values ('Video recordings for the hearing impaired');
+insert into resource_type (version) values ('Audiobook');
+insert into resource_type (version) values ('Large print');
+insert into resource_type (version) values ('Textbook');
+insert into resource_type (version) values ('Hardback book');
+insert into resource_type (version) values ('Textbook');
+insert into resource_type (version) values ('Braille Book for visually impaired');
+insert into resource_type (version) values ('eBook');
 
